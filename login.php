224fac867c4b500ea21a01bf2f6f42b690db5ebb
Vicky Kaseger <vitokaseger02@gmail.com>
2024-06-04 09:51:13 +0800

    to different page

--- login.php
+++ login.php
@@ -7,7 +7,30 @@ $db = "db_web";
 
 $data = mysqli_connect($host, $user, $password, $db);
 if($data===false) {
-    die("connection erode");
+    die("connection error");
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM login WHERE user_name = '".$username."' AND password = '".$password."' ";
+
+    $result = mysqli_query($data, $sql);
+
+    $row = mysqli_fetch_array($result);
+
+    if($row["userType"] == "user") {
+        echo "user";
+    }
+
+    elseif($row["userType"] == "admin") {
+        echo "admin";
+    }
+
+    else {
+        echo "Username or Password Incorrect";
+    }
 }
 
 
